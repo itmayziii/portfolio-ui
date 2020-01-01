3698393b3279709c21eea26a6f607f78fe676f65
itmayziii <tommymay37@gmail.com>
2019-12-31 22:31:48 -0500

    fix(gatsby plugin manifest): added missing icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter', // eslint-disable-line @typescript-eslint/camelcase
+        name: 'Full Heap Developer',
+        short_name: 'FHD', // eslint-disable-line @typescript-eslint/camelcase
         start_url: '/', // eslint-disable-line @typescript-eslint/camelcase
-        background_color: '#663399', // eslint-disable-line @typescript-eslint/camelcase
-        theme_color: '#663399', // eslint-disable-line @typescript-eslint/camelcase
-        display: 'minimal-ui'
-        // icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        background_color: '#1f2833', // eslint-disable-line @typescript-eslint/camelcase
+        theme_color: '#aaa', // eslint-disable-line @typescript-eslint/camelcase
+        display: 'standalone',
+        icon: 'src/assets/icons/fhd-logo.png' // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
